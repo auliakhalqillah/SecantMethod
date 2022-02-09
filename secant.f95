b536bdd91a5828a0faabc8a6e82356fa98885244
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2022-02-09 21:33:39 +0700

    change the error calculation

--- secant.f95
+++ secant.f95
@@ -10,7 +10,7 @@
 program secant
 implicit none
 
-real :: x1, x2, xr
+real :: xi, xf, xr
 real :: error, f
 real :: start, finish
 integer :: i
@@ -21,10 +21,10 @@ write(*,*)"---------------------------------"
 write(*,*)"SECANT METHOD - FINDING ROOT"
 write(*,*)"---------------------------------"
 write(*,*) ""
-write (*,"(a)",advance='no') "Initial xr, x1:"
-read *, x1
-write (*,"(a)",advance='no') "Final xr, x2:"
-read*, x2
+write (*,"(a)",advance='no') "Initial xr, xi:"
+read *, xi
+write (*,"(a)",advance='no') "Final xr, xf:"
+read*, xf
 
 fmt = "(a12,a13,a13,a13,a25)"
 write (*,*) ""
@@ -35,12 +35,12 @@ error = 1e-7
 i = 1
 open(unit=1, file='secant.txt',status='replace')
     info = "Not Convergence"
-    do while (abs((x2-x1)/x1) > error)
+    do while (abs((xf-xi)/xf) > error)
         ! calculate the root
-        xr = x2 - ((f(x2)*(x2-x1))/(f(x2)-f(x1)))
+        xr = xf - ((f(xf)*(xf-xi))/(f(xf)-f(xi)))
         if (isnan(xr) .or. (f(xr) > huge(f(xr))) .or. isnan(f(xr))) then
-            x1 = x2
-            x2 = xr
+            xi = xf
+            xf = xr
             i = i + 1
         else  
             if (abs(f(xr)) < error) then
@@ -48,13 +48,13 @@ open(unit=1, file='secant.txt',status='replace')
             else
                 info = "Not Convergence"
             end if
-            x1 = x2
-            x2 = xr
+            xi = xf
+            xf = xr
             i = i + 1
         end if
         ! Write the result on terimnal display and save it to file
-        write (*,*) i, xr, f(xr),info, abs((x2-x1)/x1)
-        write (1,*) i, xr, f(xr),info, abs((x2-x1)/x1)
+        write (*,*) i, xr, f(xr),info, abs((xf-xi)/xf)
+        write (1,*) i, xr, f(xr),info, abs((xf-xi)/xf)
     end do
 close(1)
 call cpu_time(finish)
